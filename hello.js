ad3ac8c77e4253aa408acf7f7b18e2125a9d164f
Tom Valverde <134085320+tvalverd@users.noreply.github.com>
2026-05-20 11:39:23 -0700

    Added the word Timestamp

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 const readline = require('readline');
 
 function showMenu() {
-	console.log(new Date().toLocaleString());
+	console.log('Timestamp' + new Date().toLocaleString());
 	console.log('Hello World');
 	console.log('Hello SCW');
 	console.log('Simple Menu Options:');
